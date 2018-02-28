d45242da338fa9b67c08785f480b3723a4b662ca
Colin Grambow <cgrambow@mit.edu>
2018-02-28 11:50:01 -0500

    Write pickling methods

--- util.py
+++ util.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import cPickle as pickle
+
 
 def custom_warning(msg, *args, **kwargs):
     return 'Warning: ' + str(msg) + '\n'
+
+
+def pickle_dump(path, obj):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def pickle_load(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
